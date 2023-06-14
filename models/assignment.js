810e813c228837715cdd4adaabd974e99ac36804
himanshurd <91275023+himanshurd@users.noreply.github.com>
2023-06-13 17:47:03 -0700

    add into model/user.js

--- models/assignment.js
+++ models/assignment.js
@@ -1,1 +1,5 @@
 //Himanshu Dhir
+const { DataTypes } = require('sequelize');
+const {submission} = require('./submission')
+const sequelize = require('../lib/sequelize');
+
--- models/user.js
+++ models/user.js
@@ -8,7 +8,7 @@ const User = sequelize.define('user', {
     name: { type: DataTypes.STRING, allowNull: false },
     email: { type: DataTypes.STRING, allowNull: false, unique: true },
     password: {type: DataTypes.TEXT, allowNull:false},
-    role: {type: DataTypes.STRING, allowNull:false}
+    role: {type: DataTypes.STRING, allowNull:false, enum: ['admin', 'instructor', 'student'], defaultValue: 'student'}
 
 })
 
