42d6b3b81c494d32291cf2688cbe518d9dabaa31
himanshurd <91275023+himanshurd@users.noreply.github.com>
2023-06-13 17:43:06 -0700

    complete implementation of user.js

--- models/user.js
+++ models/user.js
@@ -1,1 +1,23 @@
 //Himanshu Dhir
+const { DataTypes } = require('sequelize');
+const {submission} = require('./submission')
+const sequelize = require('../lib/sequelize');
+
+const User = sequelize.define('user', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false, unique: true },
+    password: {type: DataTypes.TEXT, allowNull:false},
+    role: {type: DataTypes.STRING, allowNull:false}
+
+})
+
+exports.User = User
+
+exports.UserClientFields = [
+    'id',
+    'name',
+    'email',
+    'password',
+    'role'
+  ]
