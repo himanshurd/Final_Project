ee9717d1ba097474e3888f7085c13c23ec81d121
himanshurd <91275023+himanshurd@users.noreply.github.com>
2023-06-13 18:21:02 -0700

    create a route to create a new user in api/user.js

--- api/user.js
+++ api/user.js
@@ -1,1 +1,29 @@
 //Himanshu Dhir
+const { Router } = require('express')
+
+const {ValidationError} = require('sequelize')
+const {userInfo,UserClientFields} = require('../models/user')
+const { generateAuthToken, hashAndSaltPassword, validateUser} = require('../lib/auth')
+
+/*
+* Route to create a new user.
+*/
+router.post('/', async (req, res) => {
+    try {
+      const user = await User.create(req.body, UserClientFields)
+      res.status(201).send({ id: user.id })
+      const Password = await hashAndSaltPassword(req.body.password)
+      req.body.password = Password
+    } catch (e) {
+      if (e instanceof ValidationError) {
+        res.status(400).send({ error: e.message })
+      } else {
+        throw e
+      }
+    }
+})
+
+
+
+
+
--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const { DataTypes } = require('sequelize');
 const {submission} = require('./submission')
 const sequelize = require('../lib/sequelize');
 
-const User = sequelize.define('user', {
+const userInfo = sequelize.define('user', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     name: { type: DataTypes.STRING, allowNull: false },
     email: { type: DataTypes.STRING, allowNull: false, unique: true },
