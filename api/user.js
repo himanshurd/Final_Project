20ae9892c3e0b914a56981111b11ba83e919a915
himanshurd <91275023+himanshurd@users.noreply.github.com>
2023-06-13 18:29:24 -0700

    create a route to login a registered user

--- api/user.js
+++ api/user.js
@@ -23,7 +23,26 @@ router.post('/', async (req, res) => {
     }
 })
 
-
+/*
+ * Route to login a registered user
+ */
+router.post('/login', async (req, res) => {
+    if (req.body && req.body.email && req.body.password) {
+      try {
+        const authenticated = await validateUser(req.body.email, req.body.password);
+        if (authenticated) {
+          const token = generateAuthToken(req.body.userId);
+          res.status(200).send({ token: token, message: "Successfully logged in" });
+        } else {
+          res.status(401).send({ error: "Access denied. Invalid Username or Password." });
+        }
+      } catch (err) {
+        res.status(500).send({ error: "server error. Please try again later." });
+      }
+    } else {
+      res.status(400).send({ error: "request is not vallid. Please provide correct email and password." });
+    }
+});
 
 
 
