6110ea4951a68ababd8c65dc787f4cb8193c782f
himanshurd <91275023+himanshurd@users.noreply.github.com>
2023-06-13 17:49:49 -0700

    complete implementation of model/assignment.js

--- models/assignment.js
+++ models/assignment.js
@@ -3,3 +3,19 @@ const { DataTypes } = require('sequelize');
 const {submission} = require('./submission')
 const sequelize = require('../lib/sequelize');
 
+const Assignment = sequelize.define('assignment', {
+    id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true },
+    courseId: { type: DataTypes.INTEGER, allowNull: false },
+    title: { type: DataTypes.STRING, allowNull: false },
+    points: { type: DataTypes.INTEGER, allowNull: false },
+    due: { type: DataTypes.DATE, allowNull: false }
+}) 
+exports.Assignment = Assignment
+
+exports.UserClientFields = [
+    'id',
+    'courseId',
+    'title',
+    'points',
+    'due'
+  ]
