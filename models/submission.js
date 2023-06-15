8447b39491d922169e95d10100c17bc5c503f483
himanshurd <91275023+himanshurd@users.noreply.github.com>
2023-06-14 22:10:13 -0700

    Edit model/submission according to the requirements

--- models/submission.js
+++ models/submission.js
@@ -6,14 +6,19 @@ const Submission = sequelize.define('submissions', {
     id: { type: DataTypes.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true },
     assignmentId: {type: DataTypes.INTEGER, allowNull: false},
     studentId: {type: DataTypes.INTEGER, allowNull: false},
+    timestamp: {type: DataTypes.TIMESTAMP,allowNull: false},
     grade: { type: DataTypes.FLOAT, allowNull: true },
-    file: { type: DataTypes.BLOB, length: "medium", allowNull: false }
+    file: { type: DataTypes.STRING, allowNull: false }
 })
 exports.Submission = Submission
-
 exports.SubmissionClientFields = [
     'assignmentId',
     'studentId',
+    'timestamp',
     'grade',
-    'file',
+    'file'
 ]
+
+exports.file = {
+    "text/csv": "csv",
+}
