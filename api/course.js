32934dcbb8b104e0dc06739889d348b52fcda050
KunalJhatta <Kunaljhatta22@gmail.com>
2023-06-14 21:15:18 -0700

    added needed endpoints in courses.js

--- api/course.js
+++ api/course.js
@@ -1,55 +1,44 @@
-//Kunal Jhatta
-const router = require('express').Router();
+var express = require('express');
+var router = express.Router();
 
-const { ValidationError } = require('sequelize');
-const { validateAgainstSchema } = require('../lib/validation');
-const { Course, CourseClientFields } = require('../models/course');
-const { User } = require('../models/user');
+const crypto = require("node:crypto")
+var fs = require('fs');
+const { ValidationError } = require('sequelize')
+const { Assignment, AssignmentClientFields } = require('../models/assignment')
+const { requireAuthentication } = require('../lib/auth')
+const { Submission, SubmissionClientFields, fileTypes } = require('../models/submission')
+const { Course } = require('../models/course')
+const { User } = require('../models/user')
 
-/* 
- * Route to return a list of all Courses.
-*/
-
-router.get('/', async function(req, res) {
-  let page = parseInt(req.query.page) || 1
-  page = page < 1 ? 1 : page
-  const numPerPage = 10
-  const offset = (page - 1) * numPerPage
-
-  const result = await Course.findAndCountAll({
-    limit: numPerPage,
-    offset: offset
-  })
-
-  const lastPage = Math.ceil(result.count / numPerPage)
-  const links = {}
-  if (page < lastPage) {
-    links.nextPage = `/courses?page=${page + 1}`
-    links.lastPage = `/courses?page=${lastPage}`
+const multer = require('multer');
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: `${__dirname}/../uploads`,
+    filename: (req, file, callback) => {
+        const filename = crypto.pseudoRandomBytes(16).toString("hex")
+        const extension = fileTypes[file.mimetype]
+        callback(null, `${filename}.${extension}`)
+    }
+  }),
+  fileFilter: (req, file, callback) => {
+    callback(null, !!fileTypes[file.mimetype]);
   }
-  if (page > 1) {
-    links.prevPage = `/courses?page=${page - 1}`
-    links.firstPage = '/courses?page=1'
-  }
-
-  res.status(200).json({
-    courses: result.rows,
-    pageNumber: page,
-    totalPages: lastPage,
-    pageSize: numPerPage,
-    totalCount: result.count,
-    links: links
-  })
 });
+module.exports = router;
 
 /*
- * Route to create a new Course.
+ * Route to create a new assignment.
  */
-
-router.post('/', async function (req, res) {
+router.post('/', requireAuthentication, async function (req, res, next) {
   try {
-    const course = await Course.create(req.body, CourseClientFields)
-    res.status(201).send({ id: id })
+    const assignment = await Assignment.create(req.body, AssignmentClientFields)
+    const course = await Course.findByPk(assignment.courseId)
+    const instructor = await User.findByPk(course.instructorId)
+    if(req.role == 'admin' || req.user == instructor.id) {
+      res.status(201).send({ id: assignment.id })
+    } else {
+      res.status(403).send("The request was not made by an authenticated User")
+    }
   } catch (e) {
     if (e instanceof ValidationError) {
       res.status(400).send({ error: e.message })
@@ -57,5 +46,148 @@ router.post('/', async function (req, res) {
       throw e
     }
   }
-});
+})
 
+/*
+ *  Fetch data about a specific Assignment.
+ */
+router.get('/:id', async function (req, res) {
+  const assignment = await Assignment.findByPk(req.params.id)
+  if (assignment) {
+    res.status(200).send(assignment)
+  } else {
+    res.status(404).json({"status": "not found"})
+  }
+
+})
+
+/*
+ *  Update data for a specific Assignment.
+ */
+router.patch('/:id', async function (req, res) {
+  const assignmentId = req.params.id
+  try {  
+    const result = await Assignment.update(req.body, {
+    where: { id: assignmentId },
+    fields: AssignmentClientFields
+    })
+    if (result[0] > 0) {
+      res.status(200).json({"status": "okay"})
+    } else {
+      res.status(500).send("status error")
+    }
+  } catch (err) {
+    res.status(500).send(err)
+  }
+
+})
+
+/*
+ *  Remove a specific Assignment from the database.
+ */
+router.delete('/:id', async function (req, res) {
+  const assignmentId = req.params.id
+  try {
+    const result = await Assignment.destroy({ where: { id: assignmentId }})
+    if (result > 0) {
+      res.status(204).send()
+    } else {
+      res.status(500).send("status error")
+    }
+  } catch (err) {
+    res.status(500).send(err)
+  }
+  
+})
+
+router.get('/submissions/:id/download', requireAuthentication, async function (req, res) {
+  try{
+    const submission = await Submission.findByPk(req.params.id)
+    if(!submission){
+      res.status(400).send("cannot find submission with that id")
+    }
+    var file = submission.filePath;
+    var filename = file.split('.');
+    var mimetype = submission.fileType;
+  
+    res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+    res.setHeader('Content-type', mimetype);
+  
+    var filestream = fs.createReadStream(file);
+    filestream.pipe(res);
+  } catch {
+    res.status(400).send("error downloading file.")
+  }  
+})
+
+/*
+ * Route to return a list of submissions for an assignment.
+ */
+router.get('/:id/submissions', requireAuthentication, async function (req, res) {
+  const id = req.params.id
+  try{ 
+    const assignment = await Assignment.findByPk(id)
+    const course = await Course.findByPk(assignment.courseId)
+    const instructor = await User.findByPk(course.instructorId)
+    if (req.role == 'admin' || req.user == instructor.id){
+      let page = parseInt(req.query.page) || 1
+      page = page < 1 ? 1 : page
+      const numPerPage = 10
+      const offset = (page - 1) * numPerPage
+    
+      const result = await Submission.findAndCountAll({
+        limit: numPerPage,
+        offset: offset
+      })
+      const lastPage = Math.ceil(result.count / numPerPage)
+      const links = {}
+      if (page < lastPage) {
+        links.lastPage = `/assignments/${id}/submissions?page=${lastPage}`
+        links.nextPage = `/assignments/${id}/submissions?page=${page + 1}`
+      } else if (page > 1) {
+        links.prevPage = `/assignments/${id}/submissions?page=${page - 1}`
+        links.firstPage = `/assignments/${id}/submissions?page=1`
+      } else {
+        links.firstPage = `/assignments/${id}/submissions?page=1`
+      }
+      for (const subIndex in result.rows) {
+        result.rows[subIndex].dataValues.downloadLink = `/assignments/submissions/${result.rows[subIndex].id}/download`
+      }
+      
+      res.status(200).json({
+        submissions: result.rows,
+        pageNumber: page,
+        totalPages: lastPage,
+        pageSize: numPerPage,
+        totalCount: result.count,
+        links: links
+      })
+    } else {
+      res.status(403).send("The request was not made by an authenticated User")
+    } 
+  } catch {
+    res.status(404).send(`Specified Assignment with id: ${id} not found`)
+  }
+})
+
+/*
+ * Route to create a new submission.
+ */
+router.post('/:id/submissions', requireAuthentication, upload.single('file'), async function (req, res, next) {
+  try {
+    const submission = await Submission.create({
+      'assignmentId': req.params.id,
+      'studentId': 7,
+      'grade': -1,
+      'filePath': req.file.path,
+      'fileType': req.file.mimetype
+    }, SubmissionClientFields)
+    res.status(201).send({ id: submission.id })
+  } catch (e) {
+    if (e instanceof ValidationError) {
+      res.status(400).send({ error: e.message })
+    } else {
+      throw e
+    }
+  }
+})
