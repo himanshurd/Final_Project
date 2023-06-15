ef055f322748f673ebc35e283710999e9e04b15f
himanshurd <91275023+himanshurd@users.noreply.github.com>
2023-06-15 00:35:03 -0700

    Fix an app declaration error

--- api/user.js
+++ api/user.js
@@ -1,54 +1,54 @@
-//Himanshu Dhir
-const { Router } = require('express')
+const { Router } = require('express');
+
 const router = Router();
 
-const {ValidationError} = require('sequelize')
-const {userInfo,UserClientFields} = require('../models/user')
-const { generateAuthToken, hashAndSaltPassword, validateUser, requireAuthentication} = require('../lib/auth')
+const { ValidationError } = require('sequelize');
+const { User, UserClientFields } = require('../models/user');
+const { generateAuthToken, hashAndSaltPassword, validateUser, requireAuthentication } = require('../lib/auth');
 
 /*
 * Route to create a new user.
 */
 router.post('/', async (req, res) => {
-    try {
-      const user = await User.create(req.body, UserClientFields)
-      res.status(201).send({ id: user.id })
-      const Password = await hashAndSaltPassword(req.body.password)
-      req.body.password = Password
-    } catch (e) {
-      if (e instanceof ValidationError) {
-        res.status(400).send({ error: e.message })
-      } else {
-        throw e
-      }
+  try {
+    const user = await User.create(req.body, UserClientFields);
+    res.status(201).send({ id: user.id });
+    const Password = await hashAndSaltPassword(req.body.password);
+    req.body.password = Password;
+  } catch (e) {
+    if (e instanceof ValidationError) {
+      res.status(400).send({ error: e.message });
+    } else {
+      throw e;
     }
-})
+  }
+});
 
 /*
  * Route to login a registered user
  */
 router.post('/login', async (req, res) => {
-    if (req.body && req.body.email && req.body.password) {
-      try {
-        const authenticated = await validateUser(req.body.email, req.body.password);
-        if (authenticated) {
-          const token = generateAuthToken(req.body.userId);
-          res.status(200).send({ token: token, message: "Successfully logged in" });
-        } else {
-          res.status(401).send({ error: "Access denied. Invalid Username or Password." });
-        }
-      } catch (err) {
-        res.status(500).send({ error: "server error. Please try again later." });
+  if (req.body && req.body.email && req.body.password) {
+    try {
+      const authenticated = await validateUser(req.body.email, req.body.password);
+      if (authenticated) {
+        const token = generateAuthToken(req.body.userId);
+        res.status(200).send({ token: token, message: "Successfully logged in" });
+      } else {
+        res.status(401).send({ error: "Access denied. Invalid Username or Password." });
       }
-    } else {
-      res.status(400).send({ error: "request is not vallid. Please provide correct email and password." });
+    } catch (err) {
+      res.status(500).send({ error: "server error. Please try again later." });
     }
+  } else {
+    res.status(400).send({ error: "request is not valid. Please provide correct email and password." });
+  }
 });
 
 /*
  * Route to get a user by id
  */
-app.get('/:id', requireAuthentication, async (req, res) => {
+router.get('/:id', requireAuthentication, async (req, res) => {
   const id = req.params.id;
   const user = await User.findByPk(id);
 
@@ -76,3 +76,5 @@ app.get('/:id', requireAuthentication, async (req, res) => {
     res.status(404).json({ error: `User with id: ${id} not found` });
   }
 });
+
+module.exports = router;
