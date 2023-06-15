baac610a2c09f494a0b52298d5e024280146722a
himanshurd <91275023+himanshurd@users.noreply.github.com>
2023-06-15 00:24:50 -0700

    Fix an issue of variable declaration in user.js

--- models/user.js
+++ models/user.js
@@ -1,29 +1,27 @@
-//Himanshu Dhir
 const { DataTypes } = require('sequelize');
-const {submission} = require('./submission')
+const { Submission } = require('./submission');
 const sequelize = require('../lib/sequelize');
 
 const userInfo = sequelize.define('user', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    name: { type: DataTypes.STRING, allowNull: false },
-    email: { type: DataTypes.STRING, allowNull: false, unique: true },
-    password: {type: DataTypes.TEXT, allowNull:false},
-    role: {type: DataTypes.STRING, allowNull:false, enum: ['admin', 'instructor', 'student'], defaultValue: 'student'}
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, allowNull: false },
+  email: { type: DataTypes.STRING, allowNull: false, unique: true },
+  password: { type: DataTypes.TEXT, allowNull: false },
+  role: { type: DataTypes.STRING, allowNull: false, enum: ['admin', 'instructor', 'student'], defaultValue: 'student' }
+});
 
-})
-
-exports.userInfo = userInfo
+exports.userInfo = userInfo;
 
 /*
-* one-to-many relationship between User and Submission.
-*/
-userInfo.hasMany(submission, { foreignKey: 'studentId' })
-submission.belongsTo(userInfo)
+ * One-to-many relationship between User and Submission.
+ */
+userInfo.hasMany(Submission, { foreignKey: 'studentId' });
+Submission.belongsTo(userInfo); 
 
 exports.UserClientFields = [
-    'id',
-    'name',
-    'email',
-    'password',
-    'role'
-  ]
+  'id',
+  'name',
+  'email',
+  'password',
+  'role'
+];
