c12f26b4be0577d4ba585d49dec5496bcd30ed02
himanshurd <91275023+himanshurd@users.noreply.github.com>
2023-06-15 00:14:27 -0700

    fix data type in submission.js file

--- models/submission.js
+++ models/submission.js
@@ -5,7 +5,7 @@ const Submission = sequelize.define('submissions', {
     id: { type: DataTypes.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true },
     assignmentId: {type: DataTypes.INTEGER, allowNull: false},
     studentId: {type: DataTypes.INTEGER, allowNull: false},
-    timestamp: {type: DataTypes.TIMESTAMP,allowNull: false},
+    timestamp: {type: DataTypes.DATE,allowNull: false},
     grade: { type: DataTypes.FLOAT, allowNull: true },
     file: { type: DataTypes.STRING, allowNull: false }
 })
