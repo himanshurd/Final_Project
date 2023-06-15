6fdf564c391c051786af912af9c79f20e7ce2697
himanshurd <91275023+himanshurd@users.noreply.github.com>
2023-06-15 00:12:36 -0700

    fix bugs in index.js file

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
-const router = require('express').Router();
+// const router = require('express').Router();
+const { Router } = require('express');
+const router = Router();
 
-router.use('/users', require('./users'));
-router.use('/courses', require('./courses'));
-router.use('/assignments', require('./assignments'));
+router.use('/users', require('./user'));
+router.use('/courses', require('./course'));
+router.use('/assignments', require('./assignment'));
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
