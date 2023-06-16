50b58616932bd5b48dde550cc4c2742b928c09a9
KunalJhatta <Kunaljhatta22@gmail.com>
2023-06-15 17:09:20 -0700

    updated all endpoints for users

--- api/user.js
+++ api/user.js
@@ -3,21 +3,40 @@ const router = Router();
 const { ValidationError } = require('sequelize');
 const { User, UserClientFields } = require('../models/user');
 const { generateAuthToken, hashAndSaltPassword, validateUser, requireAuthentication } = require('../lib/auth');
+const { Course, CourseUsers, CourseClientFields } = require('../models/course');
 
 /*
  * Route to create a new user.
  */
-router.post('/', async (req, res) => {
-  try {
-    const password = req.body.password;
-    const hashedPassword = await hashAndSaltPassword(password);
-    const user = await User.create({ ...req.body, password: hashedPassword }, UserClientFields);
-    res.status(201).send({ id: user.id });
-  } catch (e) {
-    if (e instanceof ValidationError) {
-      res.status(400).send({ error: e.message });
-    } else {
-      throw e;
+router.post('/', requireAuthentication, async function (req, res) {
+  const { email, password, role } = req.body;
+  if (role === 'admin' || role === 'instructor') {
+
+    try {
+      const user = await User.findOne({ where: { email: email } });
+      const authenticatedUser = await validateUser(user, password);
+
+      if (authenticatedUser && user.role === 'admin') {
+        const securePassword = await hashAndSaltPassword(req.body.password)
+        req.body.password = securePassword
+        const user = await User.create(req.body, UserClientFields)
+        res.status(201).send({ id: user.id });
+      } else {
+        res.status(403).send({
+          error: `You are not authorized to create a user with the role '${role}'.`
+        });
+      }
+    } catch (err) {
+      res.status(400).send(err);
+    }
+  } else {
+    try {
+      const securePassword = await hashAndSaltPassword(req.body.password)
+      req.body.password = securePassword
+      const user = await User.create(req.body, UserClientFields)
+      res.status(201).send({ id: user.id })
+    } catch (err) {
+      res.status(400).send(err)
     }
   }
 });
@@ -25,53 +44,77 @@ router.post('/', async (req, res) => {
 /*
  * Route to login a registered user.
  */
-router.post('/login', async (req, res) => {
+router.post('/login', async function(req, res) {
   if (req.body && req.body.email && req.body.password) {
     try {
-      const authenticated = await validateUser(req.body.email, req.body.password);
+      const user = await User.findOne({ where: { email: `${req.body.email}` } });
+      const authenticated = await validateUser(user, req.body.password);
+      
       if (authenticated) {
-        const token = generateAuthToken(req.body.userId);
-        res.status(200).send({ token: token, message: "Successfully logged in" });
+        const token = generateAuthToken(user.id, user.role);
+        res.status(200).send({
+          token: token
+        });
       } else {
-        res.status(401).send({ error: "Access denied. Invalid Username or Password." });
+        res.status(401).send({
+          error: "Invalid authentication credentials."
+        });
       }
     } catch (err) {
-      res.status(500).send({ error: "Server error. Please try again later." });
+      res.status(500).send({
+        error: "Error logging in. Try again later."
+      });
     }
   } else {
-    res.status(400).send({ error: "Request is not valid. Please provide correct email and password." });
+    res.status(400).send({
+      error: "Request body was invalid."
+    });
   }
 });
 
 /*
  * Route to get a user by id.
  */
-router.get('/:id', requireAuthentication, async (req, res) => {
-  const id = req.params.id;
-  const user = await User.findByPk(id);
-
-  if (user) {
-    let courses = [];
-    if (user.role === 'instructor') {
-      courses = await Course.findAll({
-        where: {
-          instructorId: id
-        },
-        attributes: ['id']
-      });
-    } else if (user.role === 'student') {
-      courses = await user.getCourses({
-        attributes: ['id']
+router.get('/:id', requireAuthentication, async function(req, res) {
+  const id = req.params.id
+  const user = await User.findByPk(id)
+  if (!user) {
+    res.status(400).send(`user with id: ${id} not found`)
+  } 
+  try {
+    if (req.user === id || req.role === 'admin') {
+      if (user.role === 'instructor') {
+        const coursesTaught = await Course.findAll({ where: { instructorId: id } })
+        res.status(200).send({
+          "name": user.name,
+          "email": user.email,
+          "role": user.role,
+          "courses": coursesTaught.map(course => course.id)
+        })
+      } else if (user.role === 'student') {
+        const coursesTaken = await CourseUsers.findAll({ where: { userId: id } })
+        res.status(200).send ({
+          "name": user.name,
+          "email": user.email,
+          "role": user.role,
+          "courses": coursesTaken.map(course => course.courseId) 
+        })
+      } else {
+        res.status(200).send({
+          "name": user.name,
+          "email": user.email,
+          "role": user.role
+        })
+      }
+    } else {
+      res.status(403).send({
+        error: "You are not authorized to access this information."
       });
     }
-    res.status(200).json({
-      name: user.name,
-      email: user.email,
-      role: user.role,
-      courses: courses.map(course => course.id)
+  } catch (err) {
+    res.status(404).send({
+      error: "User not found."
     });
-  } else {
-    res.status(404).json({ error: `User with id: ${id} not found` });
   }
 });
 
